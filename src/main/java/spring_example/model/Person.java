cf2de40310280b7a63027247850c5bf953b4126b
Aidyl <jgarciaalbalah@gmail.com>
2022-04-20 15:03:34 -0400

    Person: + id &  name

---
+++ src/main/java/spring_example/model/Person.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spring_example.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.UUID;
+
+/**
+ *
+ * @author Aidyl
+ */
+public class Person {
+
+    private final UUID id;
+    private final String name;
+
+    public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
